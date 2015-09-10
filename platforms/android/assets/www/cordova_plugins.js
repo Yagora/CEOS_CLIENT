b4567d61452482a4bba2b6b5cd01efbdbe143a94
unknown <cbonaudo@yahoo.fr>
2015-09-10 13:40:23 +0200

    Recréation du projet avec Cordova + Ajout des plugins (photo et geolocation) Sous-création du projet android Ajout de la page AddLokiPage avec bouton de photo (à tester)

---
+++ platforms/android/assets/www/cordova_plugins.js
@@ -0,0 +1,82 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraConstants.js",
+        "id": "cordova-plugin-camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverOptions.js",
+        "id": "cordova-plugin-camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/Camera.js",
+        "id": "cordova-plugin-camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-camera/www/CameraPopoverHandle.js",
+        "id": "cordova-plugin-camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/Acceleration.js",
+        "id": "cordova-plugin-device-motion.Acceleration",
+        "clobbers": [
+            "Acceleration"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-motion/www/accelerometer.js",
+        "id": "cordova-plugin-device-motion.accelerometer",
+        "clobbers": [
+            "navigator.accelerometer"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassError.js",
+        "id": "cordova-plugin-device-orientation.CompassError",
+        "clobbers": [
+            "CompassError"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/CompassHeading.js",
+        "id": "cordova-plugin-device-orientation.CompassHeading",
+        "clobbers": [
+            "CompassHeading"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-device-orientation/www/compass.js",
+        "id": "cordova-plugin-device-orientation.compass",
+        "clobbers": [
+            "navigator.compass"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.0",
+    "cordova-plugin-camera": "1.2.0",
+    "cordova-plugin-device-motion": "1.1.1",
+    "cordova-plugin-device-orientation": "1.0.1",
+    "cordova-plugin-geolocation": "1.0.1"
+}
+// BOTTOM OF METADATA
+});
---
+++ platforms/android/assets/www/js/common.js
@@ -0,0 +1,73 @@
+var
+socket = io.connect('http://62.210.236.194:9999/'),
+subscribe = false,
+heightPage;
+
+function initialize() {
+  heightPage =  (document.body.clientHeight);
+  document.getElementById("loginPage").style.height = heightPage + 'px';
+  document.getElementById("mapPage").style.height = heightPage + 'px';
+  document.getElementById("addLokiPage").style.height = heightPage + 'px';
+
+}
+
+function sendLogin(mess) {
+  var
+  login = document.getElementById("login").value,
+  mdp = document.getElementById("mdp").value,
+  lastName = document.getElementById("lastName").value,
+  firstName = document.getElementById("firstName").value,
+  email = document.getElementById("email").value,
+  birthday = document.getElementById("birthday").value;
+
+  if (!subscribe) {
+    socket.emit('connexion', { 'login' : login, 'mdp' : mdp });
+  }
+  else {
+    socket.emit('connexion', { 'login' : login, 'mdp' : mdp, 'lastName' : lastName, 'firstName' : firstName, 'email' : email, 'birthday' : birthday });
+  }
+  return false;
+}
+
+function display(id) {
+  var elem = document.getElementById(id),
+  btn_Connexion = document.getElementById("btn_connexion").innerHTML;
+
+  if (elem.style.display == 'none') {
+    elem.style.display = 'inline';
+    subscribe = true;
+    btn_Connexion = 'Subscribe';
+  } else {
+    elem.style.display = 'none';
+    subscribe = true;
+    btn_Connexion = 'Connexion';
+  }
+}
+
+function takePicture() {
+  navigator.camera.getPicture(function(imageURI) {
+
+    console.log(imageURI);
+
+  }, function(err) {
+
+    console.log(err);
+
+  }, cameraOptions);
+}
+
+$(document).ready( function() {
+  initialize();
+
+  socket.on('getUser', function (user) {
+    if (user.statusCode == 200) {
+      window.location = "#mapPage";
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+    if (user.statusCode == 404) {
+      console.log('kikou');
+      var html = '<div style="color:red"><p>Wrong login or password.</p></div>';
+      document.getElementById('errorConnexion').innerHTML = html;
+    }
+  });
+});
